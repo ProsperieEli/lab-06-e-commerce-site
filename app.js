b7c733805207eaeca7455b9be779db3b9d204cf3
ProsperieEli <ElijahProsperie@gmail.com>
2021-08-23 16:52:08 -0400

    Data table complete/multiple folders created

--- app.js
+++ app.js
@@ -1,5 +1,45 @@
-// import functions and grab DOM elements
-
+export const tecnoPathy = [
+  {
+    id: 1,
+    name: 'liquid metal',
+    image: './liquid-metal.jpg', 
+    description: 'Liquid mercury, safe to touch.',
+    category: 'Home business',
+    price: 2000
+  },
+  {
+    id: 2,
+    name: 'Nano-Machines: Fiber Mesh.',
+    image: './nanotech.jpg', 
+    description: 'Microscopic technology. Usable in research or other.',
+    category: 'Study',
+    price: 1500
+  },
+  {
+    id: 3,
+    name: 'Particle Accelerator',
+    image: './particles.jpg', 
+    description: 'Particle accelerator used to channel particles into beams.',
+    category: 'study',
+    price: 4000
+  },
+  {
+    id: 4,
+    name: 'Fusion Core',
+    image: './fusion-core', 
+    description: 'Nuclear fusion cell. Able to power a city for a decade.',
+    category: 'Home business',
+    price: 1000
+  }
+  {
+    id: 5,
+    name: 'Exo-Skeleton',
+    image: './exo-suit.jpg', 
+    description: 'Your very own exo-skeleton. Capable of lifting up to a thousand pounds.',
+    category: 'Home business',
+    price: 900
+  }
+];
 // initialize global state
 
 // set event listeners 
