6d1f988d62b83b8fadbed0bd0e4cff22a15faa3d
ProsperieEli <ElijahProsperie@gmail.com>
2021-08-25 19:37:05 -0400

    Finished!

--- cart/cart.js
+++ cart/cart.js
@@ -3,26 +3,36 @@ import { scienceReality } from "../science.js";
 import { renderTableRow } from "../utils.js";
 import { entireOrderTotal } from "../utils.js";
 import { getCart, clear } from "../interactive-io.js";
+import { techno } from "../product-utils.js";
 
-
+const myCart = getCart();
     
 const th = document.querySelector('tbody');
 
-for (let tech of cart) {
+const button = document.getElementById('cButton');
+const td = document.querySelector('#final'); 
+
+
+for (let tech of myCart) {
     const tr = renderTableRow(tech);
 
     th.append(tr);
 }
 
-const total = entireOrderTotal(scienceReality, cart);
+const total = entireOrderTotal(scienceReality, myCart);
 
 
 
 
-const td = document.querySelector('#final');
+
 //Arguments of entireOrderTotal are the real passing arguments
 
 td.textContent = total;
+
+button.addEventListener('click', () => {
+    clear();
+});
+
 
 
  
